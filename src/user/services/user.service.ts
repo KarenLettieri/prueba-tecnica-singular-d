caa75885140dc0c180a7376205685547b4e5746b
Karen Lettieri <karulettieri2109@gmail.com>
2023-11-11 20:38:11 -0300

    refactor: user service modified

--- src/user/services/user.service.ts
+++ src/user/services/user.service.ts
@@ -1,4 +1,4 @@
-// user.service.ts
+
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
@@ -10,6 +10,10 @@ export class UserService {
 
   async findAll(): Promise<User[]> {
     return this.userModel.find().exec();
+  }
+  
+  async findByUsername(username: string): Promise<User | null> {
+    return this.userModel.findOne({ username }).exec();
   }
 
   async findById(id: string): Promise<User> {
